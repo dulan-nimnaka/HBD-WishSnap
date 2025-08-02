7173b7c50d9575da02f4568141c5358f15d25e49
dulan-nimnaka <dulannimnaka123456@gmail.com>
2025-08-02 23:21:43 +0530

    add DOM Elements to the js

--- src/script.js
+++ src/script.js
@@ -1,3 +1,17 @@
 // src/script.js
 // DOM Elements
 
+const nameInput = doccument.getElementById('name');
+const messageInput = document.getElementById('message');
+const photoInput = document.getElementById('photo');
+const previewImage = document.getElementById('preview-image');
+const generateBtn = document.getElementById('generate-btn');
+const downloadBtn = document.getElementById('download-btn');
+const shareBtn = document.getElementById('share-btn');
+const cardPhoto = document.getElementById('card-photo');
+const cardMessage = document.getElementById('card-message');
+const confettiContainer = document.getElementById('confetti-container');
+const fileBtnText = document.getElementById('file-btn-text')
+
+const shareModel = document.querySelector('.share-model');
+const socialIcons = document.querySelectorAll('.social-icons');
